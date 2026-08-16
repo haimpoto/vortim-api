ca43de7757a3ee71afa0203a109651d050ad2ab5
חיים פוטולסקי <haimpoto@gmail.com>
2026-08-16 10:00:54 +0300

    basic routes skeleton

--- app.py
+++ app.py
@@ -9,6 +9,26 @@ app = Flask(__name__)
 def home():
     return { "status": "server is running" }, 200
 
+
+@app.route("/parshiot")
+def get_parshiot() -> dict[str, str]:
+    return {"parshiot": "parshiot"}
+
+
+@app.route("/parshiot/<parsha>/vortim")
+def get_vortim_by_parasha(parsha: str) -> dict[str, str]:
+    return {"vortim": "vortim"}
+
+
+@app.route("/parshiot/<parsha>/vortim/<int:vort_id>")
+def get_vort_by_parasha_by_id(parsha: str, vort_id: int) -> dict[str, str]:
+    return {"vort": "vort"}
+
+@app.route("/current")
+def get_current_parasha():
+    return {"parasha": "parasha"}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
